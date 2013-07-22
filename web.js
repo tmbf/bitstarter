2549d0ee06d08ebf1feba9670b5f3b832e11a9c7
ben <ben@ben-EP35-DS3.(none)>
2013-07-22 22:26:37 +0200

    Added index.html read

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf;
+    fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    response.send(new Buffer(data).toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
